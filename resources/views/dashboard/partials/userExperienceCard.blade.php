9186d5ee58de494e5152bfcee40c99afd58ccdce
successdavis <simondavid612@gmail.com>
2020-09-27 16:29:21 +0100

    display dates points where added on student dashboard

--- resources/views/dashboard/partials/userExperienceCard.blade.php
+++ resources/views/dashboard/partials/userExperienceCard.blade.php
@@ -1,18 +1,19 @@
-<nav class="panel bg-white">
+<nav class="panel bg-white" style="max-height: 400px; overflow-y: auto">
   <p class="panel-heading">
     <strong>Your Performance</strong>
   </p>
 
   @forelse ($experiences as $experience)
-    <a class="panel-block is-active">
+    <a class="panel-block is-active" >
       <span class="panel-icon">
         <i class="mdi mdi-trophy-award" aria-hidden="true"></i>
       </span>
       <div class="columns is-multiline is-mobile" style="width: 100%">
-        <div class="column is-8">{{$experience->description}}</div>
-        <div class="column is-4">{{$experience->points}}</div>
+        <div class="column is-6">{{$experience->description}}</div>
+        <div class="column is-2">{{$experience->points}}</div>
+        <div class="column is-4">{{$experience->created_at->diffForHumans()}}</div>
       </div>
-      
+
     </a>
   @empty
     <p class="section">No Experience earned yet</p>
