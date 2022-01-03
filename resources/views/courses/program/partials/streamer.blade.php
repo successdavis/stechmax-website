89ae664ebaf7cc20bd3d9599e82eff4706830ac3
successdavis <simondavid612@gmail.com>
2022-01-03 21:19:46 +0100

    Buttongs spaced evenly

--- resources/views/courses/program/partials/streamer.blade.php
+++ resources/views/courses/program/partials/streamer.blade.php
@@ -3,7 +3,9 @@
   <div class="header-content">
      <div class="header-content__hero">{{$course->title}}</div>
      <p class="header-content__paragraph">{{$course->sypnosis}}</p>
-     <a class="button subscribe-button is-success is-medium" href="{{$course->path()}}/subscription?class=false">ENROLL FOR ONLINE</a>
-     <a class="button subscribe-button is-success is-medium" href="{{$course->path()}}/subscription?class=true">ENROLL CLASSROOM</a>
+     <div class="buttons is-centered">
+       <a class="button is-success is-medium" href="{{$course->path()}}/subscription?class=false">ENROLL FOR ONLINE</a>
+       <a class="button is-success is-medium" href="{{$course->path()}}/subscription?class=true">ENROLL CLASSROOM</a>
+     </div>
   </div>
 </div>
